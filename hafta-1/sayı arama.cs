5b13f71820aa1862186b4d581239f158c7f89f81
Batuhan Aslan <batu99964@gmail.com>
2025-10-11 23:44:20 +0300

    Add files via upload
    1. hafta ödevlerim detaylı ve güzel bir şekilde yapıldı yüklendi kodların çalışma düzenleri anlaşıldı

---
+++ hafta-1/sayı arama.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] sayilar = { 3, 7, 12, 19, 25 };
+        Console.Write("Aranacak sayıyı gir: ");
+        int aranan = int.Parse(Console.ReadLine());
+
+        bool bulundu = false;
+        for (int i = 0; i < sayilar.Length; i++)
+        {
+            if (sayilar[i] == aranan)
+            {
+                bulundu = true;
+                break;
+            }
+        }
+
+        if (bulundu)
+            Console.WriteLine("Sayı dizide bulundu!");
+        else
+            Console.WriteLine("Sayı dizide yok!");
+    }
+}
---
+++ hafta-1/sayıları toplama.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] sayilar = { 5, 10, 15, 20, 25 };
+        int toplam = 0;
+
+        for (int i = 0; i < sayilar.Length; i++)
+        {
+            toplam += sayilar[i];
+        }
+
+        Console.WriteLine("Dizideki sayıların toplamı: " + toplam);
+    }
+}
